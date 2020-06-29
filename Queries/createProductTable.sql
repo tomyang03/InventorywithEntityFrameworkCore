bc6f6e90b70eaf345a775d5a3ce8627825280744
masterinex <tomyang03@gmail.com>
2020-06-29 03:08:59 -0400

    added query to create Product table

---
+++ Queries/createProductTable.sql
@@ -0,0 +1,8 @@
+﻿Create Table Products(
+ProductId BigInt Identity(1,1) Primary Key,
+Name Varchar(100) Not Null,
+Category Varchar(100),
+Color Varchar(20),
+UnitPrice Decimal Not Null,
+AvailableQuantity BigInt Not Null,
+CratedDate DateTime Default(GetDate()) Not null)
